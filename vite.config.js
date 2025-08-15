85a368a55222da907fe256b89b1848837d8d4d55
nichoth <nichoth@nichoth.com>
2025-08-14 21:10:38 -0700

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,40 @@
+import 'dotenv/config'
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import postcssNesting from 'postcss-nesting'
+import wasm from 'vite-plugin-wasm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     define: {
         global: 'globalThis'
     },
-    root: 'example',
-    plugins: [
-        preact({
-            devtoolsInProd: false,
-            prefreshEnabled: true,
-            babel: {
-                sourceMaps: 'both'
-            }
-        })
-    ],
+    root: 'example',  // https://vite.dev/config/shared-options.html#root
+    plugins: [wasm()],
     // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
     esbuild: {
         logOverride: { 'this-is-undefined-in-esm': 'silent' }
     },
-    publicDir: '_public',
+    publicDir: '_public',  // see https://vite.dev/config/shared-options#publicdir
     css: {
-        postcss: {
-            plugins: [
-                postcssNesting
-            ],
-        },
+        lightningcss: {
+            bundling: true,  // bundle css via @import
+            nesting: true,
+            // see https://lightningcss.dev/transpilation.html#custom-media-queries
+            customMedia: true,
+            minify: process.env.NODE_ENV !== 'development'
+        }
     },
     server: {
         port: 8888,
         host: true,
         open: true,
     },
+    worker: {
+        format: 'es',
+        plugins: () => [wasm()],
+    },
     build: {
-        minify: false,
+        cssMinify: 'lightningcss',
+        minify: process.env.NODE_ENV !== 'development',
         outDir: '../public',
         emptyOutDir: true,
         sourcemap: 'inline'
