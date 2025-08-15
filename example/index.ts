6ba61e0cb46175d3f4ee53ad482860fd518a2982
nichoth <nichoth@nichoth.com>
2025-08-14 20:56:20 -0700

    wip

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,65 @@
-import { type FunctionComponent, render } from 'preact'
-import { html } from 'htm/preact'
-import { example } from '../src/index.js'
+import { waitFor } from '@substrate-system/dom'
+import Debug from '@substrate-system/debug'
+import { effect } from '@substrate-system/signs'
+import { State, statusMessages } from './state.js'
+import '@substrate-system/text-input'
+const debug = Debug()
+export const PARTYKIT_HOST:string = (import.meta.env.DEV ?
+    'http://localhost:1999' :
+    'https://merge-party.nichoth.partykit.dev')
 
-example()
+/**
+ * This runs in the browser.
+ */
+const form = await waitFor('form')
+const state = State()
 
-const Example:FunctionComponent<unknown> = function () {
-    return html`<div>hello</div>`
-}
+const connector = await waitFor('.connection-status form')
 
-render(html`<${Example} />`, document.getElementById('root')!)
+connector?.addEventListener('submit', ev => {
+    ev.preventDefault()
+    debug('click')
+    const els = (ev.target as HTMLFormElement).elements
+    let docId:string = els['docuemnt-id'].value
+    // create a new document if a doc ID was not passed in
+    docId = docId || State.createDoc(state).documentId
+    State.connect(state, docId)
+})
+
+/**
+ * Set the visual status element when the connection state changes.
+ */
+effect(async () => {
+    const status = state.status.value
+    const statusElement = (document.querySelector('.connection-status'))!
+    statusElement.setAttribute('data-status', status)
+
+    const message = statusMessages[status]
+    // Update the visible text
+    const connectionText = statusElement.querySelector('.connection-text')
+    if (connectionText) {
+        connectionText.textContent = message
+    }
+
+    const text = document.querySelector('textarea')
+    text?.setAttribute('disabled', '')
+
+    debug('status.....', status)
+    if (status === 'disconnected') {
+        const text = document.querySelector('textarea')
+        text?.setAttribute('disabled', '')
+    }
+
+    // Update the visually hidden text for screen readers
+    const hiddenText = statusElement.querySelector('.visually-hidden')
+    if (hiddenText) {
+        hiddenText.textContent = `WebSocket connection status: ${message}`
+    }
+})
+
+form?.addEventListener('submit', ev => {
+    ev.preventDefault()
+    const text = (ev.target as HTMLFormElement).elements['text'] as HTMLTextAreaElement
+
+    debug('submitting the form...', text.value)
+})
---
+++ example/state.ts
@@ -0,0 +1,98 @@
+import { PartySocket } from 'partysocket'
+import {
+    IndexedDBStorageAdapter
+} from '@automerge/automerge-repo-storage-indexeddb'
+import {
+    WebSocketClientAdapter
+} from '@automerge/automerge-repo-network-websocket'
+import { type Sign, sign } from '@substrate-system/signs'
+import { type DocHandle, Repo } from '@automerge/automerge-repo'
+import Debug from '@substrate-system/debug'
+const debug = Debug()
+export const PARTYKIT_HOST:string = (import.meta.env.DEV ?
+    'http://localhost:1999' :
+    'https://merge-party2.nichoth.partykit.dev')
+
+const WS_URL = (import.meta.env.DEV ?
+    'http://localhost:1999' :
+    'https://merge-party2.nichoth.partykit.dev')
+
+export type Status = 'connecting'|'connected'|'disconnected'
+
+export type PartyState<T=any> = {
+    repo:Repo;
+    status:Sign<Status>;
+    document:Sign<DocHandle<T>|null>;
+    party:PartySocket|null;
+}
+
+export function State ():PartyState {
+    const repo = new Repo({
+        network: [
+            new WebSocketClientAdapter(WS_URL),
+        ],
+        storage: new IndexedDBStorageAdapter(),
+    })
+
+    return {
+        repo,
+        document: sign(null),
+        status: sign('disconnected'),
+        party: null
+    }
+}
+
+State.connect = function (
+    state:ReturnType<typeof State>,
+    roomId:string
+):PartySocket {
+    const party = new PartySocket({
+        host: PARTYKIT_HOST,
+        room: roomId,
+        query: { token: createHeader() }
+    })
+
+    state.party = party
+
+    party.addEventListener('open', () => {
+        debug("it's open")
+        state.status.value = 'connected'
+    })
+
+    party.addEventListener('message', ev => {
+        debug('got a message', ev.data)
+    })
+
+    party.addEventListener('close', () => {
+        debug('websocket is closed')
+        state.status.value = 'disconnected'
+    })
+
+    return party
+}
+
+/**
+ * Create a new document, return the doc handle.
+ *
+ * @param state The state object
+ * @returns The document "handle".
+ */
+State.createDoc = function<T=any> (state:ReturnType<typeof State>):DocHandle<T> {
+    const { repo } = state
+    const docHandle = repo.create<T>()
+    state.document.value = docHandle
+    return docHandle
+}
+
+export const statusMessages = {
+    connecting: 'Connecting to server...',
+    connected: 'Connected',
+    disconnected: 'Disconnected'
+}
+
+/**
+ * Placeholder
+ */
+function createHeader () {
+    return 'abc123'
+}
