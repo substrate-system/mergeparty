61f7b50b1888e21de07c296c5e564076669e0fde
nichoth <nichoth@nichoth.com>
2025-08-14 17:37:41 -0700

    wip

--- party/index.ts
+++ party/index.ts
@@ -1,10 +1,45 @@
 import type * as Party from 'partykit/server'
 
+export const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'HEAD, POST, GET, OPTIONS',
+    'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+}
+
 export default class Server implements Party.Server {
     readonly room:Party.Room
 
     constructor (room:Party.Room) {
         this.room = room
+    }
+
+    // HTTP requests
+    async onRequest (req:Party.Request): Promise<Response> {
+        if (req.method === 'OPTIONS') {
+            // respond to cors preflight requests
+            return new Response(null, {
+                status: 200,
+                headers: CORS
+            })
+        }
+
+        const url = new URL(req.url)
+
+        if (url.pathname === '/') {
+            return new Response('👍 All good')
+        }
+
+        if (url.pathname === '/health') {
+            return Response.json({
+                status: 'ok',
+                room: this.room.id,
+                repoInitialized: !!this.repo,
+                connectedPeers: Array.from(this.room.getConnections()).length
+            })
+        }
+
+        return new Response('Not Found', { status: 404 })
     }
 
     onConnect (conn: Party.Connection, ctx: Party.ConnectionContext) {
